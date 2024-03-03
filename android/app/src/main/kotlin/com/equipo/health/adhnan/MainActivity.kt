707e07f8c9b152e0d01be360afcbaba142daae40
Adhnan <adhnan.e@gmail.com>
2024-03-03 22:29:26 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/equipo/health/adhnan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.equipo.health.adhnan.equipo_health
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:equipo_health/app/app.dart';
+import 'package:equipo_health/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
